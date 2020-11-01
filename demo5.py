fb67cac2f30b498d6d0fdb7a1060fd0fe2207dba
Vusal Ismayilov <woosal@protonmail.com>
2020-11-02 01:12:25 +0300

    X

--- demo5.py
+++ demo5.py
@@ -3,26 +3,25 @@ import cv2
 import numpy as np
 
 text_to_show = "Welcome"
+nickName = "Idil"
 
-# Load image in OpenCV
+W, H = (1000, 400)
 image = cv2.imread("Backgrounds/templatefaded.png")
-
-# Convert the image to RGB (OpenCV uses BGR)
 cv2_im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-# Pass the image to PIL
 pil_im = Image.fromarray(cv2_im_rgb)
 
+font = ImageFont.truetype("Fonts/KDA.ttf", 25)
+Font = ImageFont.truetype("Fonts/KDA.ttf", 85)
 draw = ImageDraw.Draw(pil_im)
-# use a truetype font
-font = ImageFont.truetype("Fonts/KDA.ttf", 40)
 
-# Draw the text
-draw.text((200, 20), text_to_show, font=font)
+w, h = draw.textsize(text_to_show, font=font)
+nickNamew, nickNameh = draw.textsize(nickName, font=Font)
 
-# Get back the image to OpenCV
+draw.text(((W-w)/2,(H-h-200)/2), text_to_show, fill="black", font=font)
+draw.text(((W-nickNamew)/2,(H-nickNameh+50)/2), nickName, fill="black", font=Font)
+
 cv2_im_processed = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
-
+cv2.imwrite("welcome.png", cv2_im_processed)
 cv2.imshow('Fonts', cv2_im_processed)
 cv2.waitKey(0)
 
