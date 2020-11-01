2761b828cd924ea087310d933d6eeab88a4f7945
Vusal Ismayilov <woosal@protonmail.com>
2020-11-02 01:23:56 +0300

    DEMO 5

--- demo5.py
+++ demo5.py
@@ -2,27 +2,28 @@ from PIL import ImageFont, ImageDraw, Image
 import cv2
 import numpy as np
 
-text_to_show = "Welcome"
-nickName = "Idil"
+def main(nickname="WOOSAL"):
 
-W, H = (1000, 400)
-image = cv2.imread("Backgrounds/templatefaded.png")
-cv2_im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-pil_im = Image.fromarray(cv2_im_rgb)
+    text_to_show = "Welcome"
 
-font = ImageFont.truetype("Fonts/KDA.ttf", 25)
-Font = ImageFont.truetype("Fonts/KDA.ttf", 85)
-draw = ImageDraw.Draw(pil_im)
+    W, H = (1000, 400)
+    image = cv2.imread("Backgrounds/templatefaded.png")
+    cv2_im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    pil_im = Image.fromarray(cv2_im_rgb)
 
-w, h = draw.textsize(text_to_show, font=font)
-nickNamew, nickNameh = draw.textsize(nickName, font=Font)
+    font = ImageFont.truetype("Fonts/KDA.ttf", 25)
+    Font = ImageFont.truetype("Fonts/KDA.ttf", 85)
+    draw = ImageDraw.Draw(pil_im)
+    
+    w, h = draw.textsize(text_to_show, font=font)
+    nickNamew, nickNameh = draw.textsize(nickname, font=Font)
 
-draw.text(((W-w)/2,(H-h-200)/2), text_to_show, fill="black", font=font)
-draw.text(((W-nickNamew)/2,(H-nickNameh+50)/2), nickName, fill="black", font=Font)
+    draw.text(((W-w)/2,(H-h-200)/2), text_to_show, fill="black", font=font)
+    draw.text(((W-nickNamew)/2,(H-nickNameh+50)/2), nickname, fill="black", font=Font)
 
-cv2_im_processed = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
-cv2.imwrite("welcome.png", cv2_im_processed)
-cv2.imshow('Fonts', cv2_im_processed)
-cv2.waitKey(0)
+    cv2_im_processed = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
+    cv2.imwrite("welcome.png", cv2_im_processed)
+    cv2.imshow('Fonts', cv2_im_processed)
+    cv2.waitKey(0)
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
